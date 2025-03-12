2631c976bf1e43b3f98753e2e97c176133ee801d
kamukwededothy <kamukwededothy@gmail.com>
2025-03-12 06:27:12 +0000

    Add 8.cpp

---
+++ src/8.cpp
@@ -0,0 +1,11 @@
+
+int main() {
+  
+  // Generate a random number between 1 and 10
+  int randomNumber = rand() % 10 + 1;
+  
+  // Print the result
+  cout << "The random number is: " << randomNumber << endl;
+  
+  return 0;
+}
