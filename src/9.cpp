2f3e547db2123ba09b5cad6c6ef79ca291f3847a
kamukwededothy <kamukwededothy@gmail.com>
2025-03-13 20:38:04 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num1 = 0, num2 = 0;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "The sum of the numbers is: " << num1 + num2 << endl;
+    cout << "The difference of the numbers is: " << num1 - num2 << endl;
+    cout << "The product of the numbers is: " << num1 * num2 << endl;
+    cout << "The quotient of the numbers is: " << num1 / num2 << endl;
+}
